b10d81364031cf25cb1ddc70c0032b230ae4b99c
sumaaya <ilhanmohamed156@gmail.com>
2023-02-23 12:33:07 -0800

    3 commits

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ *
+ * @c: The character to write
+ *
+ * Return: On success 1, on error -1
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
