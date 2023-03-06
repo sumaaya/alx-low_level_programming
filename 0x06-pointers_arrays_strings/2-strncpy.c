2e00d555251fcffccedc855dfdd625de40c4414a
sumaaya <ilhanmohamed156@gmail.com>
2023-03-06 01:51:26 -0800

    0. memset

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- *_strncpy - function that copies a string
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
  *
- * @src: The source of strings
- * @dest: The destination of the string
- * @n: The length of int
- *
- * Return: pointer to the resulting string dest
+ * Return: pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int i;
 
-	for (i = 0; i < n && *(src + i); i++)
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
+		i++;
 	}
-	for (; i < n; i++)
+
+	while (i < n)
 	{
-	*(dest + i) = '\0';
+		dest[i] = '\0';
+		i++;
 	}
+
 	return (dest);
-
 }
